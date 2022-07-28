687c422dd384932544e2167a5c4c63dd6502db43
dragonTalon <865238022@qq.com>
2022-07-28 14:24:55 +0800

    commit

---
+++ src/main/java/com/dragon/talon/TestApplication.java
@@ -0,0 +1,11 @@
+package com.dragon.talon;
+
+/**
+ * @ClassName TestApplication
+ * @Version 1.0
+ * @Author dragon
+ * @Date 2022/7/28 2:18 下午
+ * @Description
+ **/
+public class TestApplication {
+}
